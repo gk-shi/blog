046cfa2eadf6b92d17e33c641f1ec833b4796b28
gkshi <gkshi@foxmail.com>
2020-04-03 16:33:18 +0800

    feat:增加本站介绍markdown支持

--- blog-console/.umirc.js
+++ blog-console/.umirc.js
@@ -1,24 +1,25 @@
 // ref: https://umijs.org/config/
-
 export default {
   treeShaking: true,
   proxy: {
     '/api': {
       target: process.env.DEV_API_SERVER,
       changeOrigin: true,
-      pathRewrite: { '^/api': '' }
-    }
+      pathRewrite: {
+        '^/api': '',
+      },
+    },
   },
   routes: [
     {
       path: '/login',
       component: './login/login',
-      title: '登录'
+      title: '登录',
     },
     {
       path: '/404',
       component: './404',
-      title: '登录'
+      title: '登录',
     },
     {
       path: '/',
@@ -28,48 +29,53 @@ export default {
         {
           path: '/create/:id',
           component: './create/create',
-          title: '创建博文'
+          title: '创建博文',
         },
         {
           path: '/create',
           component: './create/create',
-          title: '创建博文'
+          title: '创建博文',
         },
         {
           path: '/bless',
           component: './bless/bless',
-          title: '留言回复'
+          title: '留言回复',
         },
         {
           path: '/travel',
           component: './travel/travel',
-          title: '旅行地图'
+          title: '旅行地图',
         },
         {
           path: '/links/:id',
-          component: './links/$links'
+          component: './links/$links',
         },
         {
           path: '/setting',
           component: './setting/setting',
-          title: '个人设置'
+          title: '个人设置',
         },
         {
           path: '/list',
           component: './list/list',
-          title: '博文列表'
+          title: '博文列表',
         },
         {
           path: '/blog/:id',
           component: './blog/$blog',
-          title: '博文内容'
+          title: '博文内容',
+        },
+        {
+          path: '/website',
+          component: './website/website',
+          title: '本站介绍'
         },
         {
           path: '/',
-          component: './index'
-        }
-      ]
-    }
+          component: './index',
+        },
+      ],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
@@ -80,7 +86,7 @@ export default {
         dva: true,
         dynamicImport: {
           webpackChunkName: true,
-          loadingComponent: './components/Loading.jsx'
+          loadingComponent: './components/Loading.jsx',
         },
         title: '博客控制台',
         dll: false,
@@ -90,13 +96,14 @@ export default {
             /services\//,
             /model\.(t|j)sx?$/,
             /service\.(t|j)sx?$/,
-            /components\//
-          ]
-        }
-      }
-    ]
+            /components\//,
+          ],
+        },
+      },
+    ],
   ],
-  //umirc.js[ts]
+
+  // 拆分打包
   chainWebpack(config) {
     if (process.env.NODE_ENV === 'production') {
       config.merge({
@@ -112,110 +119,109 @@ export default {
                 name: 'vendors',
                 test: /^.*node_modules[\\/](?!ag-grid-|lodash|wangeditor|react-virtualized|rc-select|rc-drawer|rc-time-picker|rc-tree|rc-table|rc-calendar|antd|marked|for-editor|emoji-picker-react|highlight).*$/,
                 chunks: 'all',
-                priority: 10
+                priority: 10,
               },
               virtualized: {
                 name: 'virtualized',
                 test: /[\\/]node_modules[\\/]react-virtualized/,
                 chunks: 'all',
-                priority: 10
+                priority: 10,
               },
               rcselect: {
                 name: 'rc-select',
                 test: /[\\/]node_modules[\\/]rc-select/,
                 chunks: 'all',
-                priority: 10
+                priority: 10,
               },
               rcdrawer: {
                 name: 'rcdrawer',
                 test: /[\\/]node_modules[\\/]rc-drawer/,
                 chunks: 'all',
-                priority: 10
+                priority: 10,
               },
               rctimepicker: {
                 name: 'rctimepicker',
                 test: /[\\/]node_modules[\\/]rc-time-picker/,
                 chunks: 'all',
-                priority: 10
+                priority: 10,
               },
               ag: {
                 name: 'ag',
                 test: /[\\/]node_modules[\\/]ag-grid-/,
                 chunks: 'all',
-                priority: 10
+                priority: 10,
               },
               antd: {
                 name: 'antd',
                 test: /[\\/]node_modules[\\/]antd[\\/]/,
                 chunks: 'all',
-                priority: 9
+                priority: 9,
               },
               rctree: {
                 name: 'rctree',
                 test: /[\\/]node_modules[\\/]rc-tree/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               rccalendar: {
                 name: 'rccalendar',
                 test: /[\\/]node_modules[\\/]rc-calendar[\\/]/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               rctable: {
                 name: 'rctable',
                 test: /[\\/]node_modules[\\/]rc-table[\\/]es[\\/]/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               wang: {
                 name: 'wang',
                 test: /[\\/]node_modules[\\/]wangeditor[\\/]/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               marked: {
                 name: 'marked',
                 test: /[\\/]node_modules[\\/]marked[\\/]/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               forEditor: {
                 name: 'marked',
                 test: /[\\/]node_modules[\\/]for-editor[\\/]/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               emojiPickerReact: {
                 name: 'emojiPickerReact',
                 test: /[\\/]node_modules[\\/]emoji-picker-react[\\/]/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               highLight: {
                 name: 'highLight',
                 test: /[\\/]node_modules[\\/]highlight[\\/]/,
                 chunks: 'all',
-                priority: -1
+                priority: -1,
               },
               lodash: {
                 name: 'lodash',
                 test: /[\\/]node_modules[\\/]lodash[\\/]/,
                 chunks: 'all',
-                priority: -2
-              }
-            }
-          }
-        }
-      })
-    }
-    //过滤掉momnet的那些不使用的国际化文件
+                priority: -2,
+              },
+            },
+          },
+        },
+      });
+    } //过滤掉momnet的那些不使用的国际化文件
+
     config
       .plugin('replace')
       .use(require('webpack').ContextReplacementPlugin)
       .tap(() => {
-        return [/moment[/\\]locale$/, /zh-cn/]
-      })
-  }
-  // ignoreMomentLocale: true
-}
+        return [/moment[/\\]locale$/, /zh-cn/];
+      });
+  }, // ignoreMomentLocale: true
+};
